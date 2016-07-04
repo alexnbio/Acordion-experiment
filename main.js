9da3ace3a0b3e4a03da0c04b979d238fd10af086
Alexandru Nagacevschi <alexbio2008@yahoo.com>
2016-07-04 20:12:04 +0300

    make new modifications in index.html and main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function($) {
 	//code here
+	var elem = $(".aContent");
+	$(".aTitle").on("click", function() {
+		if ( $(this).hasClass("active")) {
+			$(this).toggleClass("active").next(elem).toggle();
+		} else {
+			$(".aTitle.active").toggleClass("active").next(elem).toggle();
+			$(this).toggleClass("active").next(elem).toggle();
+		}
+		
+	});
 });
